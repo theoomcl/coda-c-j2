0c988d44d5382dbfa69c3f418cb87b153f636bf1
théoomcl <theo.michel@coda-student.school>
2024-09-26 12:01:59 +0200

    exercice 5

---
+++ ex-5/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{
+
+    int nombre_1=12;
+    int nombre_2=7;
+    int resultat= (nombre_1 + nombre_2)/2;
+    printf("La moyenne de 12 et 7 est égale à %.2f\n", "Théo");
+
+
+    exit(0);
+}
