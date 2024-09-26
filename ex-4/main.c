5bd6351aed2b5efa185e87bdbd95cf0a3aa35cb3
théoomcl <theo.michel@coda-student.school>
2024-09-26 11:51:25 +0200

    exercice 4

---
+++ ex-4/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{
+
+    int nombre=5;
+    int nombre2=5;
+    int resultat= nombre * nombre2;
+    printf("%d x %d = %d\n", nombre , nombre2 , 
+        resultat);
+
+
+    exit(0);
+}
